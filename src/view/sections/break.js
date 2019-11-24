78352e417f1d06793176fec09e3da8f57e4c37f8
keithporcaro <keith.porcaro@gmail.com>
2019-11-24 15:14:11 -0500

    icon and grammar update on break

--- src/view/sections/break.js
+++ src/view/sections/break.js
@@ -4,7 +4,7 @@ const Break = {
     view: (vnode)=>{
         return [
             m("p", `You've made good progress so far! Why not take a short coffee break?`),
-            m("p", [`While you're waiting for the coffee to brew, check out `, m("a", {href:"https://spotlightproject.gitlab.io/"}, "Spotlight"), ` a collaboration of UNI Global Unions and The Guardian Project.`]),
+            m("p", [`While you're waiting for the coffee to brew, check out `, m("a", {class: "link b hover-blue", href:"https://spotlightproject.gitlab.io/"}, "Spotlight"), `, a collaboration of UNI Global Unions and The Guardian Project.`]),
             ]
     }
 }
