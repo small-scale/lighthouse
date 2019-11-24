c756e5dcbe72c3f13a35cbb36106a5e660b80883
keithporcaro <keith.porcaro@gmail.com>
2019-11-24 15:07:16 -0500

    build config tinker 2

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    publicPath: '/assets/',
+    publicPath: '/',
     liveReload: true,
     port: 8888
   },
