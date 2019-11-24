66a3abf9ea6b0b05373b52704f9ec6017d85af7c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-24 19:51:47 +0000

    Deploy keithporcaro/lighthouse to github.com/keithporcaro/lighthouse.git:gh-pages

--- src/model/storage.js
+++
@@ -1,9 +0,0 @@
-import localforage from "localforage"
-
-localforage.config({
-    name        : 'limelight',
-    storeName   : 'limelight', // Should be alphanumeric, with underscores.
-    description : 'limelight'
-});
-
-export default localforage
--- todo.md
+++
@@ -1,5 +0,0 @@
-* results page
-    - Report Card
-    - Print
-* print
-* cleanup style on mobile
